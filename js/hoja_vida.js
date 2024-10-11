76796638387c615f58a2bcd8e8801359073bef35
Carlos <carlos.yaruro@assisprex.com>
2024-10-10 19:09:04 -0500

    Mi quinto  Commit + archivo js

--- js/hoja_vida.js
+++ js/hoja_vida.js
@@ -9,3 +9,31 @@ function registrar() {
     console.log("click en el boton Registrar");
 
 }
+
+/*
+
+    crear la RAMA 
+    creo en el local 
+    git branch dev 
+    hago los cambios  //add commit 
+
+    una vez hecho los cambios, la primera vez 
+    creo mi rama remota 
+    Comando: 
+    git branch -M dev  
+    hago los cambios  // add commit  --->  ami rama remota dev 
+
+    ------------------------
+    con el comando 
+    git checkout cambio de rama.  
+    git checkout main 
+
+    siempre debemos trabajar en la rem DEV y desde dev enviao a main 
+     git  checkout main 
+     git merge dev main   esto licua o combina los arcvhivos y lo que esta en dev pasa a main 
+     git push -u origin main  
+     
+     
+
+
+*/ 
